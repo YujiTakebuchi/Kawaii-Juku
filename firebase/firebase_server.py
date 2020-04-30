c671a55592207bd417129eb6f0b9de5a0c3a8cf3
YujiTakebuchi <takeyou.swift@gmail.com>
2020-04-30 14:07:39 +0900

    register roommember

--- firebase/firebase_server.py
+++ firebase/firebase_server.py
@@ -11,6 +11,7 @@ db = firestore.client()
 
 # User登録
 def register_user(id, discordId, twitterId, accountType, name):
+    print(firebase.firestore.collection('_').doc().id)
 
     doc_ref = db.collection(u'User').document()
     doc_ref.set({
@@ -38,6 +39,28 @@ def register_user(id, discordId, twitterId, accountType, name):
 
 
 # RoomMember登録
+def register_room_member(id, user_id, room_id):
+    
+    doc_ref = db.collection(u'RoomMember').document()
+    doc_ref.set({
+                u'id': id,
+                u'userId': user_id,
+                u'roomId': room_id,
+                u'isBlocked': False,
+                u'isAdmin': False
+                })
+
+
+def register_room_member(id, user_id, room_id, is_blocked, is_admin):
+    
+    doc_ref = db.collection(u'RoomMember').document()
+    doc_ref.set({
+                u'id': id,
+                u'userId': user_id,
+                u'roomId': room_id,
+                u'isBlocked': is_blocked,
+                u'isAdmin': is_admin
+                })
 
 # RoomMember取得
 
@@ -46,5 +69,12 @@ def register_user(id, discordId, twitterId, accountType, name):
 # RoomMember削除
 
 
-# 未登録User登録
+# 未登録User確認
+
+def register_unknown_user(discord_id, name):
+
+    if (check_is_user_registered(discord_id)):
+
+    else :
+
 
