1e7adc4eaf7df631c768920917338b126e12f6a0
YujiTakebuchi <takeyou.swift@gmail.com>
2020-05-06 07:01:40 +0900

    C#版discordbotのサンプルプロジェクト（VS上での動作確認のみ)

---
+++ c_sharp/DiscordbotCSsharp/DiscordbotCSsharp/DiscordbotCSharp.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+public static class DiscordbotCSharp
+{
+    private static void Main() => MainAsync().Wait();
+
+    private static async Task MainAsync()
+    {
+        var client = new DiscordSocketClient();
+        var token = Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN"); ;
+
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+
+        client.MessageReceived += OnMessageReceived;
+
+        Console.ReadLine();
+    }
+
+    private static async Task OnMessageReceived(SocketMessage arg)
+    {
+        if (arg.Author.IsBot == true)
+        {
+            return;
+        }
+        await arg.Channel.SendMessageAsync(arg.Content);
+        Console.WriteLine(arg.Content.ToString());
+    }
+}
