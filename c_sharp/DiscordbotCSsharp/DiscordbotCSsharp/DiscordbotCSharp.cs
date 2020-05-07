89d81ce66aba9266d4eddf8c27eab1c9c0f4082b
YujiTakebuchi <takeyou.swift@gmail.com>
2020-05-08 08:25:37 +0900

    ディスコードボットとUnityの連携を試みた（ダメだった）

--- c_sharp/DiscordbotCSsharp/DiscordbotCSsharp/DiscordbotCSharp.cs
+++ c_sharp/DiscordbotCSsharp/DiscordbotCSsharp/DiscordbotCSharp.cs
@@ -1,32 +1,44 @@
 ﻿using Discord;
 using Discord.WebSocket;
 using System;
+using System.IO;
 using System.Threading.Tasks;
+using discordbot;
+using UnityEngine;
 
-public static class DiscordbotCSharp
+namespace discordbot
 {
-    private static void Main() => MainAsync().Wait();
-
-    private static async Task MainAsync()
+    public static class DiscordbotCSharp
     {
-        var client = new DiscordSocketClient();
-        var token = Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN"); ;
+        private static void Main() => MainAsync().Wait();
 
-        await client.LoginAsync(TokenType.Bot, token);
-        await client.StartAsync();
-
-        client.MessageReceived += OnMessageReceived;
-
-        Console.ReadLine();
-    }
-
-    private static async Task OnMessageReceived(SocketMessage arg)
-    {
-        if (arg.Author.IsBot == true)
+        private static async Task MainAsync()
         {
-            return;
+            var client = new DiscordSocketClient();
+            var token = Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN"); ;
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            client.MessageReceived += OnMessageReceived;
+
+            Console.ReadLine();
         }
-        await arg.Channel.SendMessageAsync(arg.Content);
-        Console.WriteLine(arg.Content.ToString());
+
+        private static async Task OnMessageReceived(SocketMessage arg)
+        {
+            if (arg.Author.IsBot == true)
+            {
+                return;
+            }
+            await arg.Channel.SendMessageAsync(arg.Content);
+            File.WriteAllText(@"C:\Users\Chamboo\UnityProjects\New Unity Project\Assets\comment.txt", arg.Content.ToString());
+
+            //DiscordbotCS.generateTextFile(arg.Content.ToString());
+
+            //Comment.GetInstance().setCommentText(arg.Content.ToString());
+            //DiscordbotCS.dataLog();
+            Console.WriteLine(arg.Content.ToString());
+        }
     }
 }
